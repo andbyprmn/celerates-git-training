a799623a21b296b22e85447a94ab9e0c7d1e876e
inggridmd <inggridmelanica@gmail.com>
2019-05-16 06:22:52 +0700

    algoritma sorting

---
+++ branch/sorting_inggrid.cpp
@@ -0,0 +1,33 @@
+
+#include<iostream>
+#include<stdio.h>
+
+int main(){
+    int a,b,c,d,e,jumlah;
+    int nilai[6];
+    
+printf("------ PROGRAM SORTING ------ \n\n");
+printf("Input Random Nilai Sebanyak 6 kali \n");
+    for (a=0; a<=5; a++){
+        printf("Nilai ke [%d] : ",a+1);
+        scanf("%d",&nilai[a]);
+    }
+    printf("\nNilai masukan User\n");
+    for (b=0; b<=5; b++){
+        printf("%d, ",nilai[b]);
+    }
+    printf("\n\nHasil Urut dari kecil ke besar \n");
+    for (c=0; c<=5; c++){
+        for (d=c+1; d<=5; d++) {
+            if (nilai[c]>nilai[d]){
+                jumlah=nilai[c];
+                nilai[c]=nilai[d];
+                nilai[d]=jumlah;
+            }
+        }
+    }
+    for (e=0; e<=5; e++){
+        printf("%d, ",nilai[e]);
+    }
+    return 0;
+}
