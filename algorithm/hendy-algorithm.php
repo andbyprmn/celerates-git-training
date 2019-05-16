d2ae04f78ed16fbd261da33a0b30c54bd5ce5073
jawahendy <hendyn25@gmail.com>
2019-05-16 11:12:49 +0700

    tugas sort php

--- algorithm/hendy-algorithm.php
+++ algorithm/hendy-algorithm.php
@@ -2,3 +2,10 @@
     1.  Saya memiliki angka acak 1,4,5,3,2,8
         Tolong urutkan dengan metode sorting 
 -->
+<?php
+$angka = array("1","4","5","3","2","8");
+sort($angka);
+
+print_r($angka);
+
+?>
